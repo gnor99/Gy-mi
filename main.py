07210038df17c20157fe09bc05c66b3cd76105b2
gnor99 <gnor9696@gmail.com>
2021-11-04 10:23:42 +0100

    kommenteltem

--- main.py
+++ main.py
@@ -2,23 +2,30 @@
 import random
      
 def deckgenerator():
-    fruits = ['apple','grapes', 'strawberry', 'peach', 'plum', 'pear']
+
+    fruits = ['apple','grapes', 'strawberry', 'peach', 'plum', 'pear'] #ennyi gyümölcs van
+
     deck = []
+
     for i in range(6): #Ennyi gyümölcs van
         for j in range(5): #Számozás
             for k in range(4): # Mindegyik kártyából ennyi van
-                deck.append(fruits[i] + ' ' + str(j+1))
+                deck.append(fruits[i] + ' ' + str(j+1)) #létrehoz egy kátyát úgy hogy elöször szerepel a gyümölcs és utána hogy hányas számú a kártya
+
+    random.shuffle(deck) #megkeveri a kártyákat
+
     return deck
 
 
 def draw(deck):
+
     a = deck.pop(0) #kiveszi az elso kartyát a paklibol aka huz egy lapot
+
     return a
 
 
 def main():
     deck = deckgenerator()
-    random.shuffle(deck) #megkeveri a kártyákat
     print(draw(deck)) 
 
 
