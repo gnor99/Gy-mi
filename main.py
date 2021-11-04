bf0d36bd5d35ed10b7ea579954c7dc132e587a92
gnor99 <gnor9696@gmail.com>
2021-11-04 11:31:41 +0100

    kiegészittetem azzal hogy mind a két paklibol huz egyet egyet majd azok közül kiválaszt egyet

--- main.py
+++ main.py
@@ -5,38 +5,64 @@ def deckgenerator():
 
     fruits = ['apple','grapes', 'strawberry', 'peach', 'plum', 'pear'] #ennyi gyümölcs van
 
-    deck = []
+    deck1 = []
+    deck2 = []
 
     for i in range(6): #Ennyi gyümölcs van
         for j in range(5): #Számozás
             for k in range(4): # Mindegyik kártyából ennyi van
-                deck.append(fruits[i] + ' ' + str(j+1)) #létrehoz egy kátyát úgy hogy elöször szerepel a gyümölcs és utána hogy hányas számú a kártya
+                deck1.append(fruits[i] + ' ' + str(j+1)) #létrehoz egy kátyát úgy hogy elöször szerepel a gyümölcs és utána hogy hányas számú a kártya
 
-    random.shuffle(deck) #megkeveri a kártyákat
-    print(len(deck))
+    random.shuffle(deck1) #megkeveri a kártyákat
 
-    for i in range(39):
-        deck.pop(0)
+    for i in range(40): #az egyszemélyes játéknál 40 lapot vissza kell tenni a dobozba
+        deck1.pop(0)
 
-    print(len(deck))
+    for i in range(40): #ketté szedjük a paklit
+        deck2.append(deck1.pop(0)) 
+
+    return deck1, deck2
+
+def choose(left, right):
+    #a két felhuzzpt lapból választani kell egyet amit megtarunk, a másikat eldobjuk
+    print("type left if you want this card: " + left + '\n' + "type right if you want this card: " +  right)
+    
+
+    while input() != "left" or input() !="right":
+        inp = input()
+
+        if inp == "left":
+            return left
+
+        elif inp == "right":
+            return right
+    
 
 
-    return deck
+def draw(deck1, deck2):
+    first = deck1.pop(0) #kiveszi az elso kartyát az elsö paklibol aka huz egy lapot
+    sec = deck2.pop(0) #kiveszi az elso kartyát a masodik paklibol aka huz egy lapot
+    print("first: " + first + " sec: " + sec)
+    choosen = choose(first, sec) 
 
+    return choosen
 
-def draw(deck):
-
-    a = deck.pop(0) #kiveszi az elso kartyát a paklibol aka huz egy lapot
-
-    return a
-
-
-
+def stackgenerator(): #létrehozza a különböző tárolokat 
+    #ezek a gyümölcs paklik
+    
+    appledeck = [] 
+    grapesdeck = []
+    strawberrydeck = []
+    peachdeck = []
+    plumdeck = []
+    peardeck = []
 
 
 def main():
-    deck = deckgenerator()
-    print(draw(deck)) 
+    deck1,deck2 = deckgenerator()
+    stackgenerator()
+    print(draw(deck1,deck2)) 
+
 
 
 if __name__ == "__main__":
